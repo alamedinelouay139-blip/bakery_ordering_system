a31491f95c3f3d7fd9493f903e5412840cc868d1
Louay <alamedinelouay139@gmail.com>
2026-01-03 07:59:45 -0800

    Enable MySQL SSL for Railway database

--- index.js
+++ index.js
@@ -15,8 +15,10 @@ const db = mysql.createConnection({
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
-  port: process.env.MYSQL_PORT
+  port: process.env.MYSQL_PORT,
+  ssl: { rejectUnauthorized: true }
 });
+
 
 // Signup route
 app.post("/auth/signup", async (req, res) => {
